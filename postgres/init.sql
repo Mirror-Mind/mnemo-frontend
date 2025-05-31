a76f53497194af6ee7c1215dee6f4fb99d7c5b14
Ishaan Shah <70190533+ishaan812@users.noreply.github.com>
2025-06-01 04:13:30 +0530

    Update init.sql

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,5 +1,33 @@
+-- Create the mnemo user if it doesn't exist
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'mnemo') THEN
+        CREATE ROLE mnemo WITH LOGIN PASSWORD 'mnemo_password';
+    END IF;
+END
+$$;
+
+-- Create the mnemo_db database if it doesn't exist
+SELECT 'CREATE DATABASE mnemo_db OWNER mnemo'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'mnemo_db')\gexec
+
+-- Grant necessary permissions to mnemo user
+GRANT ALL PRIVILEGES ON DATABASE mnemo_db TO mnemo;
+
+-- Connect to the mnemo_db database to set up extensions
+\c mnemo_db
+
 -- Enable pgvector extension
 CREATE EXTENSION IF NOT EXISTS vector;
+
+-- Grant schema permissions to mnemo user
+GRANT ALL ON SCHEMA public TO mnemo;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO mnemo;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO mnemo;
+
+-- Set default privileges for future objects
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO mnemo;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO mnemo;
 
 -- Create indexes for better performance with vector operations
 -- This can be uncommented when you have vector columns in your tables
